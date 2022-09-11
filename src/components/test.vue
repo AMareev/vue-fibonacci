65986d27d27799de19b9f671290f817948c16b79
AMareev <113265213+AMareev@users.noreply.github.com>
2022-09-11 11:53:46 +0300

    Add files via upload

---
+++ src/components/test.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="headers">
+        <h1>TEST</h1>
+    </div>
+    <br/>
+    <div class="inputs-container">
+        <div>{{ inputValue }}</div>
+    </div>
+
+   <!--<canvas></canvas>-->
+</template>
+
+<script>
+export default {
+    name: "test",
+    props: {
+        inputValue: {
+            type: String
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
